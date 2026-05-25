350fb9c5d0f96ca820b88f1aa9c9f3d66c21a426
AamirrH <aamirr.1704@gmail.com>
2026-05-26 01:30:52 +0530

    feat : add API Gateway for centralised routing between different microservices

---
+++ API-Gateway/src/test/java/com/code/prodapp/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.prodapp.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
